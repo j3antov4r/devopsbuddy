900dd396a42005eb8d35c23c5ea34d586e630de9
jean <jeantovar@gmail.com>
2018-04-22 20:19:55 -0400

    Added about page and styles for it

--- src/test/java/com/devopsbuddy/DevopsbuddyApplicationTests.java
+++ src/test/java/com/devopsbuddy/DevopsbuddyApplicationTests.java
@@ -1,5 +1,6 @@
 package com.devopsbuddy;
 
+import com.devopsbuddy.utils.MD5Util;
 import com.devopsbuddy.web.i18n.I18NService;
 import org.junit.Assert;
 import org.junit.Test;
@@ -7,6 +8,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import sun.security.provider.MD5;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -15,11 +17,20 @@ public class DevopsbuddyApplicationTests {
 	@Autowired
 	I18NService i18NService;
 
+
+
 	@Test
 	public void testMessageByLocaleService() {
 		String expectedResult = "Bootstrap starter template";
 		String messageId = "index.main.callout";
 		String actual = i18NService.getMessage(messageId);
 		Assert.assertEquals("The actual and expected Strings doesn't match", expectedResult, actual);
+	}
+
+	@Test
+	public void testMD5gravatar() {
+		String email= "jeantovar@gmail.com";
+		String hash= MD5Util.md5Hex(email);
+		System.out.println("jean MD5 => "+ hash);
 	}
 }
