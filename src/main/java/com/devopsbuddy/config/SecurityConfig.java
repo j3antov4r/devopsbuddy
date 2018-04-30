cb29042e68c541f55b8a52bf468a9d75262f4a04
jean <jeantovar@gmail.com>
2018-04-29 23:14:02 -0400

    added Security Authorization and authentication features with Spring-Thymeleaf

---
+++ src/main/java/com/devopsbuddy/config/SecurityConfig.java
@@ -0,0 +1,55 @@
+package com.devopsbuddy.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    public static final String[] PUBLIC_MATCHERS = {
+        "/webjars/**",
+        "/css/**",
+        "/js/**",
+        "/images/**",
+        "/",
+        "/about/**",
+        "/contact/**",
+        "/error/**/*"
+    };
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http
+                .authorizeRequests()
+                .antMatchers(PUBLIC_MATCHERS).permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/login").defaultSuccessUrl("/payload")
+                .failureUrl("/login?error").permitAll()
+                .and()
+                .logout().permitAll();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
+
+        //esta linea la agregué yo para que funcionara la autenticacion, ya que
+        //pide que haya un encoder para el password
+        PasswordEncoder encoder =
+                PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        auth
+                .inMemoryAuthentication()
+                .withUser("user").password(encoder.encode("password"))
+                .roles("USER");
+    }
+
+
+}
